57c255f876334cfe671dbeb9318d9a4fef752e8f
nithingowdahm87 <nithingowdahm87@github.com>
2026-09-10 23:28:07 +0000

    large added

--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" { default = "ap-south-1" }
 variable "project_name" { default = "octabyte-nithin" }
 variable "environment" { default = "staging" }
-variable "instance_type" { default = "t3.medium" }
+variable "instance_type" { default = "m7i-flex.large" }
 variable "staging_subnet_id" { 
   type = string
   default = "" 

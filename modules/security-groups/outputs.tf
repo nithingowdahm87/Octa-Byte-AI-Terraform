c8ac1c2f2b3e200c6c2cbe28e20d89f3eeab5dec
nithingowdahm87 <nithingowdahm87@github.com>
2026-09-10 08:53:09 +0000

    Fix bastion sg count reference in outputs

--- modules/security-groups/outputs.tf
+++ modules/security-groups/outputs.tf
@@ -2,4 +2,4 @@ output "alb_sg_id" { value = aws_security_group.alb.id }
 output "ec2_sg_id" { value = aws_security_group.app.id }
 output "rds_sg_id" { value = aws_security_group.rds.id }
 output "vpc_endpoints_sg_id" { value = aws_security_group.vpc_endpoints.id }
-output "bastion_sg_id" { value = aws_security_group.bastion.id }
+output "bastion_sg_id" { value = length(aws_security_group.bastion) > 0 ? aws_security_group.bastion[0].id : "" }

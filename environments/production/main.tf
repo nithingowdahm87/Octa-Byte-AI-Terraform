1cf2da5afb98d087599e20689288e19f12285df2
nithingowdahm87 <nithingowdahm87@github.com>
2026-09-10 08:33:59 +0000

    Add missing github_environment to github-oidc module

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -204,4 +204,5 @@ module "github-oidc" {
   github_org              = "nithingowdahm87"
   github_repository       = "Octa-Byte-AI-Application"
   github_branch           = "main"
+  github_environment      = "production"
 }

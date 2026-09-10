7895f4a65484dd3060e502d94d3d0b9920c56a9d
nithingowdahm87 <nithingowdahm87@github.com>
2026-09-10 11:59:18 +0000

    Allow SonarQube port 9000 inbound to staging EC2

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -92,6 +92,15 @@ resource "aws_security_group" "staging_sg" {
     description = "Allow SSH inbound"
   }
 
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SonarQube inbound"
+  }
+
+
 
   egress {
     from_port   = 443
